056b4ab12aefad7b46b8aacd379444d8cb7dc206
yuhijackman <yuuhiandlisa@gmail.com>
2023-10-07 09:24:35 +0900

    Add auth

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,13 @@
+import Logo from "./logo";
+
+const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
